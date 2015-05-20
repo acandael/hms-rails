4d723b5873d38fe6aa59d66ccd0e854c792c4122
acandael <anthony.candaele@ugent.be>
2015-05-20 16:55:56 +0200

    implement themes on homepage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def index
     @articles = NewsArticle.order(created_at: :desc).limit(2)
+    @themes = Theme.all
   end
 end
--- spec/features/visitor_views_homepage_spec.rb
+++ spec/features/visitor_views_homepage_spec.rb
@@ -4,6 +4,8 @@ describe "viewing the homepage" do
   let!(:article1) { NewsArticle.create!(newsarticle_attributes(title: "article1")) }
   let!(:article2) { NewsArticle.create!(newsarticle_attributes(title: "article2")) }
   let!(:article3) { NewsArticle.create!(newsarticle_attributes(title: "article3")) }
+  let!(:theme1) { Theme.create!(title: "theme 1", description: "description for theme 1") }
+  let!(:theme2) { Theme.create!(title: "theme 2", description: "description for theme 2") } 
   
   before do
     visit root_path
@@ -23,5 +25,9 @@ describe "viewing the homepage" do
     expect(page).to have_text article2.body[0..90]
     expect(page).to have_text article3.title
     expect(page).to have_text article3.body[0..90]
+    expect(page).to have_text theme1.title
+    expect(page).to have_text theme1.description
+    expect(page).to have_text theme2.title
+    expect(page).to have_text theme2.description
   end
 end
