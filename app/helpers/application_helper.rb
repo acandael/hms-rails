3b874a162d157e3b655154b5e8cc23a4d3d9c0c3
acandael <anthony.candaele@ugent.be>
2015-05-26 10:57:15 +0200

    add meta title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
   def page_title
     if content_for?(:title)
-      "AdLit - #{content_for(:title)}"
+      "Health, Media & Society - #{content_for(:title)}"
+    end
+  end
+
+  def page_description
+    if content_for?(:description)
+      content_for(:description)
     end
   end
 end
