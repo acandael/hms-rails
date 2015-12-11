a625e518c02f103d85c7cce403cb78664b9cbe79
acandael <anthony.candaele@ugent.be>
2015-12-11 14:30:05 +0100

    implement image for mission page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def index
     @articles = NewsArticle.order(created_at: :desc).limit(2)
-    @themes = Theme.all.order(title: :asc)
+    @themes = Theme.all.order(title: :desc)
   end
 end
