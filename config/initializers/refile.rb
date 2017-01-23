70b696c7e6b712e15f68c3b4a926a37ce7dfa930
acandael <anthony.candaele@ugent.be>
2017-01-23 08:45:04 +0100

    fix wrong bucket

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
   aws = {
     access_key_id: Rails.application.secrets.AWS_ACCESS_KEY_ID, 
     secret_access_key: Rails.application.secrets.AWS_SECRET_ACCESS_KEY, 
-    bucket: "adlit"
+    bucket: "hmsocio"
   }
 
   Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
