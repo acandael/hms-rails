617ff61ef8a320f46255d50c6aace6cb0cc9002b
acandael <anthony.candaele@ugent.be>
2017-02-01 08:45:23 +0100

    implement twitterfeed

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,6 +1,6 @@
 class ThemesController < ApplicationController
   def index
-    @themes = Theme.includes(:members).all
+    @themes = Theme.includes(:members).all.order(title: :desc)
   end
 
   def show
