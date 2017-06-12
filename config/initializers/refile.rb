12204c3756d1ab4ce3232da15484284b3a4871b3
acandael <anthony.candaele@ugent.be>
2017-06-12 14:17:08 +0200

    store assets on s3

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,14 +1,14 @@
 require "refile/backend/s3"
 
-# if Rails.env.production?
-#   aws = {
-#     access_key_id: Rails.application.secrets.AWS_ACCESS_KEY_ID, 
-#     secret_access_key: Rails.application.secrets.AWS_SECRET_ACCESS_KEY, 
-#     region: "eu-west-1",
-#     bucket: "hmsocio"
-#   }
-#
-#   Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
-#   Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
-# end
+if Rails.env.production?
+  aws = {
+    access_key_id: Rails.application.secrets.AWS_ACCESS_KEY_ID, 
+    secret_access_key: Rails.application.secrets.AWS_SECRET_ACCESS_KEY, 
+    region: "eu-west-1",
+    bucket: "hmsocio"
+  }
+
+  Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
+  Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
+end
      
